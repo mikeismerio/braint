ac040bb8cf04707789e1e4345001713eaf9c6534
Isleg <mikeismerio@gmail.com>
2025-03-03 16:41:48 -0600

    Update model.py

--- model.py
+++ model.py
@@ -1,9 +1,11 @@
 import os
+import sys
 
 model_path = "2025-19-02_VGG_model.h5"
 
-# Verificar si el archivo existe
 if os.path.exists(model_path):
     print(f"El archivo {model_path} existe y tiene un tamaño de {os.path.getsize(model_path) / (1024*1024):.2f} MB")
+    sys.stdout.flush()
 else:
     print("El archivo no existe o está corrupto.")
+    sys.stdout.flush()
