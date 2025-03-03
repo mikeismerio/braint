e4d7f88c80e7c09b0b5a61b03ac2d80a3401408d
Isleg <mikeismerio@gmail.com>
2025-03-03 16:42:19 -0600

    Update model.py

--- model.py
+++ model.py
@@ -1,11 +1,4 @@
-import os
-import sys
+import streamlit as st
 
-model_path = "2025-19-02_VGG_model.h5"
-
-if os.path.exists(model_path):
-    print(f"El archivo {model_path} existe y tiene un tamaño de {os.path.getsize(model_path) / (1024*1024):.2f} MB")
-    sys.stdout.flush()
-else:
-    print("El archivo no existe o está corrupto.")
-    sys.stdout.flush()
+st.write("Modelo cargado correctamente.")
+st.text(model.summary())
