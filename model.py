c864e95f2b08ef9c7a6970c7af9117b28d59861f
Isleg <mikeismerio@gmail.com>
2025-03-03 16:37:37 -0600

    Update model.py

--- model.py
+++ model.py
@@ -4,21 +4,9 @@ from tensorflow.keras.models import load_model
 # Ruta del archivo del modelo
 model_path = "2025-19-02_VGG_model.h5"
 
-# Cargar el modelo
-model = load_model(model_path)
+# Cargar el modelo sin compilar
+model = load_model(model_path, compile=False)
 
 # Resumen del modelo
 print("Resumen del modelo:")
 model.summary()
-
-# Obtener información de los pesos
-print("\nCapas y pesos del modelo:")
-for layer in model.layers:
-    print(f"Capa: {layer.name}, Tipo: {layer.__class__.__name__}")
-    for weight in layer.weights:
-        print(f"  - {weight.name}, Forma: {weight.shape}")
-
-# Revisar la configuración del modelo
-config = model.get_config()
-print("\nConfiguración del modelo:")
-print(config)
